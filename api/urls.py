0dfeb4600e61219247c20638bbe4e7d801094b7b
MiguelVides10 <miguel.vides@catolica.edu.sv>
2023-11-21 23:00:59 -0600

    Metodos

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,7 @@ from api import views
 
 urlpatterns= [
     path('usuarios/<str:correo>/<str:contrasenia>/', views.UsuariosView.as_view(),name='user'),
+    path('usuariopost/',views.UsuarioCreateView.as_view(), name='crearuser'),
+    path('enfermedades/', views.EnfermesadViewList.as_view(), name='enfermedades'),
+    path('menus/<int:id_perfil>',views.MenuViewList.as_view(), name='menus'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,22 @@
 from typing import Any
-from django import http
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
 from .serializer import *
 from django.utils.decorators import method_decorator
 from rest_framework.views import View
 from django.http.response import JsonResponse
+import json
+from django.db import connection
+from rest_framework import generics
 # Create your views here.
+
+def dictfetchall(cursor):
+    desc = cursor.description
+    return [
+        dict(zip([col[0] for col in desc], row))
+        for row in cursor.fetchall()
+    ]
+
 
 class UsuariosView(View):
     
@@ -20,5 +30,43 @@ class UsuariosView(View):
             credenciales = usuarios[0]
             datos ={ 'message':'success', 'usuarios':credenciales}
         else:
-            datos ={'messages':'Usuarios no encontrados'}
+            datos ={'error':'Usuario no encontrados'}
         return JsonResponse(datos)
+    
+
+class UsuarioCreateView(generics.CreateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+    def post(self,request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            datos ={'message':'usuario creado'}
+            return JsonResponse(datos)
+        else:
+            datos ={'message':'No se guardo la info'}
+            return JsonResponse(datos)
+        
+class EnfermesadViewList(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args: Any, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request):
+        enfermedades = list(Enfermedad.objects.values())
+        if len(enfermedades)>0:
+            datos ={ 'message':'success', 'enfermedades':enfermedades}
+        else:
+            datos ={'error':'No hay datos'}
+        return JsonResponse(datos)
+    
+class MenuViewList(View):
+    def get(self, request, id_perfil):
+        lookup_field = 'id_perfil'
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT desayuno, almuerzo, cena FROM hidiet.menu m inner join hidiet.perfil p on p.id_menu = m.id_menu where p.id_perfil = %s;", [id_perfil])
+            rows = dictfetchall(cursor)
+            datos ={ 'message':'success', 'menu':rows}
+            return JsonResponse(datos)
+
+
